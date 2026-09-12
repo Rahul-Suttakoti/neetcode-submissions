7234d47c38c201dc45809f04edd96aaae8bd4c7a
Rahul-Suttakoti <rahulsuttakoti@gmail.com>
2026-09-12 20:53:42 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+     if(s.size()!=t.size()) return false;
+
+     unordered_map<int,int> mpp;
+
+     for(auto it:s){
+        mpp[it]++;
+     }   
+
+     for(auto it:t){
+        mpp[it]--;
+     }
+
+     for(auto it:mpp){
+        if(it.second!=0) return false;
+     }
+
+     return true;
+    }
+};
