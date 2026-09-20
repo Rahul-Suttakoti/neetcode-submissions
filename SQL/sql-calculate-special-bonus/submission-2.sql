0590fa82ba1139b359b150933c51c74f8f72a5a5
Rahul-Suttakoti <rahulsuttakoti@gmail.com>
2026-09-20 22:35:51 +0530

    Add: sql-calculate-special-bonus - submission-2

---
+++ SQL/sql-calculate-special-bonus/submission-2.sql
@@ -0,0 +1,8 @@
+SELECT employee_id AS employee_id,
+       CASE
+           WHEN employee_id % 2 = 1 AND name NOT LIKE 'M%'
+           THEN salary
+           ELSE 0
+       END AS bonus
+FROM Employees
+order by employee_id;
